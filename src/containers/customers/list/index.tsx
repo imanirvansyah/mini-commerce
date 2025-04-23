2e2629a9e4ca56c87efca27edcd0ad311f655f66
imanirvansyah <imanirvansyaah@gmail.com>
2025-04-23 15:43:18 +0700

    update customers list

--- src/containers/customers/list/index.tsx
+++ src/containers/customers/list/index.tsx
@@ -1,93 +1,133 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { FilterIcon } from "lucide-react";
 import Link from "next/link";
+
 const ListCustomers = () => {
   return (
     <div>
       <div className="w-full h-full overflow-scroll mt-12">
         <div className="flex items-center justify-start gap-2 mb-3">
-          <input placeholder="search customer.." className="p-2" />
-          <button className="bg-blue-500 text-white p-2 rounded">Search</button>
-          <button className="border text-white p-2 rounded">Filter</button>
+          <Input placeholder="Search customer here..." className="w-fit" />
+          <Button>Search</Button>
+          <Button variant={"outline"} size={"icon"}><FilterIcon /></Button>
         </div>
-        <table className="table-auto w-full">
-          <thead>
-            <tr>
-              <th className="text-start">Name</th>
-              <th className="text-start">Email</th>
-              <th className="text-start">Total Purchased</th>
-              <th className="text-start">Last Purchase</th>
-              <th className="text-start">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>John doe</td>
-              <td>johndoe@mail.com</td>
-              <td>Rp1.000.000</td>
-              <td>January 13, 2025</td>
-              <td>
-                <Link href="/customers/1">
-                  <button className="bg-blue-500 text-white p-2 rounded" >View</button>
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>John doe</td>
-              <td>johndoe@mail.com</td>
-              <td>Rp1.000.000</td>
-              <td>January 13, 2025</td>
-              <td>
-                <Link href="/customers/1">
-                  <button className="bg-blue-500 text-white p-2 rounded" >View</button>
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>John doe</td>
-              <td>johndoe@mail.com</td>
-              <td>Rp1.000.000</td>
-              <td>January 13, 2025</td>
-              <td>
-                <Link href="/customers/1">
-                  <button className="bg-blue-500 text-white p-2 rounded" >View</button>
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>John doe</td>
-              <td>johndoe@mail.com</td>
-              <td>Rp1.000.000</td>
-              <td>January 13, 2025</td>
-              <td>
-                <Link href="/customers/1">
-                  <button className="bg-blue-500 text-white p-2 rounded" >View</button>
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>John doe</td>
-              <td>johndoe@mail.com</td>
-              <td>Rp1.000.000</td>
-              <td>January 13, 2025</td>
-              <td>
-                <Link href="/customers/1">
-                  <button className="bg-blue-500 text-white p-2 rounded" >View</button>
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>John doe</td>
-              <td>johndoe@mail.com</td>
-              <td>Rp1.000.000</td>
-              <td>January 13, 2025</td>
-              <td>
-                <button className="bg-blue-500 text-white p-2 rounded">View</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <Card>
+          <Table className="w-full">
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Email</TableHead>
+                <TableHead>Total Purchased</TableHead>
+                <TableHead>Last Purchase</TableHead>
+                <TableHead>Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>John doe</TableCell>
+                <TableCell>johndoe@mail.com</TableCell>
+                <TableCell>Rp1.000.000</TableCell>
+                <TableCell>January 13, 2025</TableCell>
+                <TableCell>
+                  <Link href="/customers/1">
+                    <Button size="sm">View</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>John doe</TableCell>
+                <TableCell>johndoe@mail.com</TableCell>
+                <TableCell>Rp1.000.000</TableCell>
+                <TableCell>January 13, 2025</TableCell>
+                <TableCell>
+                  <Link href="/customers/1">
+                    <Button size="sm">View</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>John doe</TableCell>
+                <TableCell>johndoe@mail.com</TableCell>
+                <TableCell>Rp1.000.000</TableCell>
+                <TableCell>January 13, 2025</TableCell>
+                <TableCell>
+                  <Link href="/customers/1">
+                    <Button size="sm">View</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>John doe</TableCell>
+                <TableCell>johndoe@mail.com</TableCell>
+                <TableCell>Rp1.000.000</TableCell>
+                <TableCell>January 13, 2025</TableCell>
+                <TableCell>
+                  <Link href="/customers/1">
+                    <Button size="sm">View</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>John doe</TableCell>
+                <TableCell>johndoe@mail.com</TableCell>
+                <TableCell>Rp1.000.000</TableCell>
+                <TableCell>January 13, 2025</TableCell>
+                <TableCell>
+                  <Link href="/customers/1">
+                    <Button size="sm">View</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <div className="flex my-4">
+            <TablePagination />
+          </div>
+        </Card>
       </div>
     </div>
   );
+}
+
+export function TablePagination() {
+  return (
+    <Pagination className="w-fit">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
 }
 
 export default ListCustomers;
